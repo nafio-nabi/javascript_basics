11f560edbcae9f68aa81b4e623e6dae10e9db620
Nafio Nabi <nafionabi@Nafios-MacBook-Air.local>
2025-06-29 12:21:39 +0600

    Functions

---
+++ 6_functions.js
@@ -0,0 +1,75 @@
+// A function is a reuseable block of code that performs a specific task. Instead of repeating the same block of code, you write it once in a function, and then call the function when you need it.
+
+// Declare a function
+function helloWorld() {
+    console.log("Hello World!");
+}
+
+// Invoke/call the function.
+helloWorld();
+
+
+// Declare a function with parameters.
+function greetUser(name) {
+    console.log("Hello, " + name + "!");
+}
+
+// Invoke/call the function.
+greetUser("John");
+
+// Declare a function that returns a value.
+function addition(a, b) {
+    return (a + b);
+}
+
+// Invoke/call the function.
+let result = addition(2, 2)
+console.log(result);
+
+function double(num) {
+    return (num * 2);
+}
+
+// Invoke/call the function.
+console.log(double(5));
+
+function calculateArea(width, height) {
+    let area = width * height;
+    return area;
+}
+
+// Invoke/call the function.
+console.log("Area is " + calculateArea(2, 4));
+
+function isEven(number) {
+    if (number % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Invoke/call the function.
+console.log(isEven(10));
+console.log(isEven(7));
+
+// Function expression.
+let multiply = function(a, b) {
+    return (a * b);
+}
+
+// Invoke/call the function.
+console.log(multiply(2, 2));
+
+// Arrow function.
+const greet = (name) => {
+    console.log("Hi, " + name + "!");
+};
+
+// Invoke/call the function.
+greet("John");
+
+const square = x => x * x;
+
+// Invoke/call the function.
+console.log(square(5));
