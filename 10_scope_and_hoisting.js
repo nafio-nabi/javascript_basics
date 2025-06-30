bbd42b44f12208eadd458f44a1ffa5387efdfc0c
Nafio Nabi <nafionabi@Nafios-MacBook-Air.local>
2025-06-30 11:40:40 +0600

    Scope and hoisiting

--- 10_scope_and_hoisting.js
+++ 10_scope_and_hoisting.js
@@ -1,17 +1,21 @@
-// Scope refers to where in your code you can access variables. There are main types of scope: global, block, function.
+// Scope refers to where in your code you can access variables.
+// There are three main types of scope: global, block, and function.
 
-// Global scope: a variable declared outside any function or block. It can be accessed anywhere in your code.
-let name = "John"
+// Global scope: variables declared outside any function or block.
+// Variables with global scope can be accessed anywhere in your code.
+let userName = "John";
 
-function greet() {
-    console.log("Hello " + name);
+function greetUser() {
+    console.log("Hello, " + userName);
 }
 
-greet();
+greetUser();
 
-// Function scope: a variable declared inside a function. It can only be accessed within that function.
+// Function scope: variables declared inside a function.
+// Variables with function scope can be accesed only inside the function they are declared in.
+// Variables declared with let have function scope.
 function helloWorld() {
-    let message = "Hello World!";
+    let message = "Hello, World!";
     console.log(message);
 }
 
@@ -19,42 +23,59 @@ helloWorld();
 // This will throw an error.
 // console.log(message);
 
-// Block scope: variables declared with let and const inside a block. It can only be accesed within that block.
-function addition() {
-    let a = 2;
-    let b = 5;
-    console.log(a + b);
+// Block scope: variables declared, with let and const, inside a block.
+// Variables declared, with let and const, inside a block can be accessed only inside the block they are declared in.
+function add() {
+    let x = 10;
+    let y = 2;
+    const result = x + y;
+    console.log(result);
 }
 
-addition();
-// This will throw an error.
-// console.log(a);
+add();
+// These will throw errors.
+// console.log(x);
+// console.log(result);
 
-// Hoisting: moving declarations to the top of their scope before the code runs.
+// Hoisting is JavaScript's behaviour of moving declarations to the top of their scope before the code runs.
 
 // Function declaration are hoisted.
-console.log(multiplication(2, 2));
+multiplication(2, 2);
 
 function multiplication(numOne, numTwo) {
-    return (numOne * numTwo);
+    let result = numOne * numTwo;
+    console.log(result);
 }
 
-// Var is hoisted but not initalized.
-console.log(a);
-var a = 5;
+// Variables declared with var is declared but not initialized.
+console.log(x);
+var x = 10;
 
-// let and const are not fully hoisted.
-// This will throw an error.
-// console.log(b);
-// const b = 10;
+// Variables declared with let and const are hoisted but not initialized.
+// These will throw errors.
+// console.log(y);
+// let y = 2;
+// console.log(z);
+// const z = 5;
 
-function testScope() {
+// function testScope() {
+//     if (true) {
+//         var a = 1;
+//         let b = 2;
+//     }
+//     console.log(a);
+//     console.log(b);
+// }
+
+// testScope();
+
+function scopeTest() {
     if (true) {
-        var a = 1;
-        var b = 2;
+        let x = 10;
+        var y = 20;
     }
-    console.log(a);
-    console.log(b);
+    console.log(typeof x);
+    console.log(typeof y);
 }
 
-testScope();
+scopeTest();
